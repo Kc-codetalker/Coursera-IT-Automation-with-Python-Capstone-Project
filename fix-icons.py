1902ea680677f8adb879188ed83a2acea6095c46
KCC_Playmaker <kevinc.27999@gmail.com>
2021-03-08 11:18:21 +0700

    Added few comments to describe script.

--- fix-icons.py
+++ fix-icons.py
@@ -3,9 +3,12 @@
 import os
 from PIL import Image
 
+# Describe directory path for input and output
 in_path = 'images'
 out_path = '/opt/icons'
 ext = '.jpeg'
+
+# Load file names
 entries = os.listdir(in_path)
 
 try:
@@ -14,6 +17,7 @@ except Exception as e:
   print(e)
 
 for entry in entries:
+  # Describe file path for input and output
   entry_path = os.path.join(os.getcwd(), in_path, entry)
   save_path = os.path.join(out_path, entry+ext)
 
